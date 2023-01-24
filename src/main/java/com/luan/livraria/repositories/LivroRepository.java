f2ac438c8ca1a0e476555e1139d32fdd4b813735
MurukLuan <murukluan@gmail.com>
2023-01-24 16:48:06 -0300

    iniciando as interfaces repository

---
+++ src/main/java/com/luan/livraria/repositories/LivroRepository.java
@@ -0,0 +1,13 @@
+package com.luan.livraria.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.luan.livraria.domain.Livro;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer>{
+	
+	
+
+}
