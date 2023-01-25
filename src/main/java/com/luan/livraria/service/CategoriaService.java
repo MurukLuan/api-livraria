d09f4ebf01800a3a3f716caf9da2f096a8a93b35
MurukLuan <murukluan@gmail.com>
2023-01-24 23:17:53 -0300

    exceção personalizada para objeto não encontrado

--- src/main/java/com/luan/livraria/service/CategoriaService.java
+++ src/main/java/com/luan/livraria/service/CategoriaService.java
@@ -2,11 +2,14 @@ package com.luan.livraria.service;
 
 import java.util.Optional;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.luan.livraria.domain.Categoria;
+import com.luan.livraria.exceptions.ObjectNotFoundException;
 import com.luan.livraria.repositories.CategoriaRepository;
+
 
 @Service
 public class CategoriaService {
@@ -16,7 +19,7 @@ public class CategoriaService {
 	
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!" + id));
 	}
 
 }
