29e3207de5aefd5ab52fc56e6b4f669c745eca30
MurukLuan <murukluan@gmail.com>
2023-01-25 00:21:28 -0300

    Exceção personalizda para deletar categorias que possuem liros

--- src/main/java/com/luan/livraria/service/CategoriaService.java
+++ src/main/java/com/luan/livraria/service/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.luan.livraria.domain.Categoria;
@@ -42,7 +43,13 @@ public class CategoriaService {
 
 	public void delete(Integer id) {
 		findById(id);
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new com.luan.livraria.exceptions.DataIntegrityViolationException("Categoria não pode ser deletada, possui livros associados");
+			
+		}
+		
 		
 	}
 }
