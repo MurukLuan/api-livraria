45dd176257d35ccc01a0f1115fa00a2dfdc82804
MurukLuan <murukluan@gmail.com>
2023-01-25 00:03:01 -0300

    update para categoria

--- src/main/java/com/luan/livraria/service/CategoriaService.java
+++ src/main/java/com/luan/livraria/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.luan.livraria.domain.Categoria;
+import com.luan.livraria.dtos.CategoriaDTO;
 import com.luan.livraria.exceptions.ObjectNotFoundException;
 import com.luan.livraria.repositories.CategoriaRepository;
 
@@ -28,6 +29,14 @@ public class CategoriaService {
 	
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
+		return repository.save(obj);
+	}
+
+	public Categoria update(Integer id, CategoriaDTO objDto) {
+		Categoria obj = findById(id);
+		obj.setNome(objDto.getNome());
+		obj.setDescricao(objDto.getDescricao());
+		
 		return repository.save(obj);
 	}
 }
