38761a7c8ca3aaa6592edf3a19e5e83d7f38bb8b
MurukLuan <murukluan@gmail.com>
2023-01-25 01:01:32 -0300

    criando dto e metodo get para livros com categoria para fazer a listagem

--- src/main/java/com/luan/livraria/resources/LivroResource.java
+++ src/main/java/com/luan/livraria/resources/LivroResource.java
@@ -1,13 +1,18 @@
 package com.luan.livraria.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.luan.livraria.domain.Livro;
+import com.luan.livraria.dtos.LivroDTO;
 import com.luan.livraria.service.LivroService;
 
 @RestController
@@ -22,6 +27,14 @@ public class LivroResource {
 		Livro obj = service.findById(id);
 		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<LivroDTO>> findAll(@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat){
+		List<Livro> list = service.findAll(id_cat);
+		List<LivroDTO> listDto = list.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
+	
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
--- src/main/java/com/luan/livraria/service/LivroService.java
+++ src/main/java/com/luan/livraria/service/LivroService.java
@@ -1,5 +1,6 @@
 package com.luan.livraria.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +16,18 @@ public class LivroService {
 	@Autowired
 	private LivroRepository repository;
 	
+	@Autowired
+	private CategoriaService categoriaService;
+	
 	public Livro findById(Integer id) {
 		Optional<Livro> obj = repository.findById(id);
 
 		return obj.orElseThrow(()-> new ObjectNotFoundException("Livro não encontrado com o id: " + id));
+	}
+
+	public List<Livro> findAll(Integer id_cat) {
+		categoriaService.findById(id_cat);
+		return repository.findAllByCategoria(id_cat);
 	}
 
 }
