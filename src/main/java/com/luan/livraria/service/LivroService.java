06051aa1afefdc220687c536314d55652c9e9138
MurukLuan <murukluan@gmail.com>
2023-01-25 01:33:03 -0300

    adicionando o post para livro

--- src/main/java/com/luan/livraria/service/LivroService.java
+++ src/main/java/com/luan/livraria/service/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.luan.livraria.domain.Categoria;
 import com.luan.livraria.domain.Livro;
 import com.luan.livraria.exceptions.ObjectNotFoundException;
 import com.luan.livraria.repositories.LivroRepository;
@@ -42,5 +43,15 @@ public class LivroService {
 		newObj.setTexto(obj.getTexto());
 		
 	}
+
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		
+		obj.setCategoria(cat);
+		return repository.save(obj);
+	}
+
+	
 
 }
